002fd9ca75875516d58c6a89e146d0494432d583
Nachtw8 <94112184+Nachtw8@users.noreply.github.com>
2021-12-22 20:52:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+console.log('Hello Winc Academy students')
+let resultaat=0;
+
+/* this is a large sum 
+resultaat = 1230941 * 1823794;
+console.log(resultaat);
+
+resultaat2 = 637263 / 54 ;
+console.log (resultaat2);
+*/
